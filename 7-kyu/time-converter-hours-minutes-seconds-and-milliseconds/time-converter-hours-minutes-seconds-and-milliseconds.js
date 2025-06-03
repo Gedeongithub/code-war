07a7682c8c3ecd44fb42ae62ea2331685da2b5f5
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-06-03 09:48:18 +0200

    Add solution - CodeHub

---
+++ 7-kyu/time-converter-hours-minutes-seconds-and-milliseconds/time-converter-hours-minutes-seconds-and-milliseconds.js
@@ -0,0 +1,10 @@
+function convert(date){
+  //Your code here!
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+​
+  return `${hours}:${minutes}:${seconds},${milliseconds}`;
+}
+​
