c4b35d08e7c49ac2d31f09d2becc8691d6b0921a
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-06-14 15:30:44 +0200

    Update solution - CodeHub

--- 8-kyu/tip-calculator/tip-calculator.js
+++ 8-kyu/tip-calculator/tip-calculator.js
@@ -1,12 +1,19 @@
+ 
 function calculateTip(amount, rating) {
-    rating = rating.toLowerCase();
-    switch (rating) {
-        case 'terrible': return Math.ceil(amount * 0);
-        case 'poor': return Math.ceil(amount * 0.05);
-        case 'good': return Math.ceil(amount * 0.10);
-        case 'great': return Math.ceil(amount * 0.15);
-        case 'excellent': return Math.ceil(amount * 0.20);
-        default: return 'Rating not recognised';
-    }
+  rating = rating.toLowerCase();
+​
+  if (rating === 'terrible') {
+    return Math.ceil(amount * 0);
+  } else if (rating === 'poor') {
+    return Math.ceil(amount * 0.05);
+  } else if (rating === 'good') {
+    return Math.ceil(amount * 0.10);
+  } else if (rating === 'great') {
+    return Math.ceil(amount * 0.15);
+  } else if (rating === 'excellent') {
+    return Math.ceil(amount * 0.20);
+  } else {
+    return 'Rating not recognised';
+  }
 }
 ​
