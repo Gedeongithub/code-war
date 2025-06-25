1e0ac52866285c53c7235fa92c77eab7f5203707
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-06-25 08:53:06 +0200

    Add solution - CodeHub

---
+++ 7-kyu/sentences-with-functions/sentences-with-functions.js
@@ -0,0 +1,10 @@
+const Adam =(a)=> a !== undefined? 'Adam '+a :'Adam.';
+const has =(a)=> a !== undefined ? 'has '+a : 'has';
+const a =(a)=>a !== undefined ? 'a '+ a : 'a';
+const dog =(a)=> a!== undefined ? 'dog ' + a: 'dog.';
+const The =(a)=> a !== undefined ? 'The '+a : 'The' ;
+const name =(a)=> a !== undefined ? 'name '+a : 'name';
+const of =(a)=> a !== undefined ? 'of '+ a : 'of';
+const the =(a)=> a !== undefined ? 'the '+a : 'the';
+const is=(a)=> a !== undefined ? 'is '+a : 'is';
+const also =(a)=> a !== undefined ? 'also '+a : 'also';
