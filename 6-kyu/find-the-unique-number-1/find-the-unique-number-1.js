c36be5bdbeb53ee9d646331c038f47b418f4f9f5
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-06-25 10:34:31 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-unique-number-1/find-the-unique-number-1.js
+++ 6-kyu/find-the-unique-number-1/find-the-unique-number-1.js
@@ -1,2 +1,2 @@
 const findUniq =(arr)=> Object.entries(arr.reduce((acc,cur)=>{ acc[cur]=(acc[cur]||0)+1
-    return acc; },{})).filter(([key,value])=>value ==1).join('')[0];
+    return acc; },{})).filter(([key,value])=>value <2).join('')[0];
