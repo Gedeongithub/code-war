6891d19813a53ec25cb5faef531f77c5da6a5179
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-06-26 08:37:11 +0200

    Update solution - CodeHub

--- 8-kyu/return-two-highest-values-in-list/return-two-highest-values-in-list.js
+++ 8-kyu/return-two-highest-values-in-list/return-two-highest-values-in-list.js
@@ -1,6 +1,4 @@
-const twoHighest =(arr)=> {
-    return  Object.keys(arr.sort((a,b)=>b-a).reduce((acc,cur)=>{
-        acc[cur]=(acc[cur]||0)+1;
-        return acc
-    },{})).slice(-2).map(val=>Number(val)).sort((a,b)=>b-a);
+ 
+function twoHighest(arr) {
+    return [...new Set(arr)].sort((a,b)=>b-a).slice(0,2)
 }
