7110c644883834815102935d6fb6f943971309a5
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-06-27 11:58:04 +0200

    Update solution - CodeHub

--- 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.js
+++ 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.js
@@ -1,16 +1,2 @@
-const solution =(number)=>{
-  if(number <0) return 0;
-  if(number == 4)return 3;
-​
-  let sum =[];
-  for(let i=0;i<number;i++){
-    if(i%3==0||i%5==0){
-      sum.push(i)
-    }
-  }
-  return sum.reduce((a,b)=>a+b,0)
-}
-​
-// console.log(solution(10))
-​
-​
+ 
+const solution =n=>n<1?0:[...new Array(n).keys()].filter(val=>val%3==0||val%5==0).reduce((acc,cur)=>acc+cur,0);
