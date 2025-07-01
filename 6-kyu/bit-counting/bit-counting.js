d91ca91893b1399ac741e99b88d0b63dc236d46e
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-01 14:40:19 +0200

    Update solution - CodeHub

--- 6-kyu/bit-counting/bit-counting.js
+++ 6-kyu/bit-counting/bit-counting.js
@@ -1,9 +1,9 @@
 function countBits(n) {
-  if(n==undefined) return 0;
+​
   let obj= [...n.toString(2)]
 obj =obj.reduce((acc,cur)=>{
       acc[cur]? acc[cur]++:acc[cur]=1;
       return acc
   },{})
-  return +obj['1']
+  return +obj['1']? obj[1]:0
 }
