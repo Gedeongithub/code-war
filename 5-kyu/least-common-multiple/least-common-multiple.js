cb6296a6bbe13dedd7119de5a91844d0abe3f5c6
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-01 15:25:45 +0200

    Update solution - CodeHub

--- 5-kyu/least-common-multiple/least-common-multiple.js
+++ 5-kyu/least-common-multiple/least-common-multiple.js
@@ -1,6 +1,11 @@
  
-function lcm(...numbers) {
-  if(numbers.length == 0) return 1;
-  
-    return numbers.reduce((acc,cur)=>cur==0? 0:(acc*+cur)/acc- +cur,1)
+const gcd =(a,b)=> b-a
+const lcm =(a,b)=>{
+    return (a*b)/gcd(a,b)
+}
+​
+function leastCommonMultiple(...numbers){
+    if(numbers.length==0) return 1;
+    if(numbers.includes(0)) return 0;
+    return numbers.reduce((acc,cur)=>lcm(acc,cur),1)
 }
