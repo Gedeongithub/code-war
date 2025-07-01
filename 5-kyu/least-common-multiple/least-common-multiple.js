139980b91bc90167bc7306850c9c5cc8a2bd1689
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-01 15:05:21 +0200

    Update solution - CodeHub

--- 5-kyu/least-common-multiple/least-common-multiple.js
+++ 5-kyu/least-common-multiple/least-common-multiple.js
@@ -1,5 +1,6 @@
+ 
 function lcm(...numbers) {
   if(numbers.length == 0) return 1;
   
-    return numbers.reduce((acc,cur)=>cur==0? 0:acc*cur,1)/6
+    return numbers.reduce((acc,cur)=>cur==0? 0:(acc*cur)/acc-cur,1)
 }
