fafc6fa55640c89042ff25ab265d9b3b00925f5c
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-03 15:31:01 +0200

    Update solution - CodeHub

--- 6-kyu/insert-value-into-an-array/insert-value-into-an-array.js
+++ 6-kyu/insert-value-into-an-array/insert-value-into-an-array.js
@@ -1,17 +1,7 @@
  
-// Array.prototype.insert = function(index,val){
-//     let arr = this;
-//     if(val>arr.length){
-//         arr.push(val);
-//         return arr;
-//     };
-//     arr.splice(index,0,val);
-//     return arr
-// }
-​
 Object.defineProperty(Array.prototype,'insert',{
   value:function(index,val){
-    if( val >= this.length){
+    if( index >= this.length){
         this.push(val);
     }else{
     this.splice(index,0,val);
@@ -20,17 +10,3 @@ Object.defineProperty(Array.prototype,'insert',{
 },
   enumerable:false
 })
-​
-​
-// Object.defineProperty(Array.prototype, 'insert', {
-//   value: function(index, value) {
-//     if (index >= this.length) {
-//       this.push(value);
-//     } else {
-//       this.splice(index, 0, value);
-//     }
-//     return this;
-//   },
-//   enumerable: false
-// });
-​
