59552b8472e7b0cb6619c34824637ec45bff4ddf
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-03 15:27:46 +0200

    Update solution - CodeHub

--- 6-kyu/insert-value-into-an-array/insert-value-into-an-array.js
+++ 6-kyu/insert-value-into-an-array/insert-value-into-an-array.js
@@ -9,29 +9,29 @@
 //     return arr
 // }
 ​
-// Object.defineProperty(Array.prototype,'insert',{
-//   value:function(index,val){
-//     let arr = this;
-//     if(val>arr.length){
-//         arr.push(val);
-//         return arr;
-//     };
-//     arr.splice(index,0,val);
-//     return arr
-// },
-//   enumerable:false
-// })
+Object.defineProperty(Array.prototype,'insert',{
+  value:function(index,val){
+    let arr = this;
+    if(val>arr.length){
+        arr.push(val);
+        return arr;
+    };
+    arr.splice(index,0,val);
+    return arr
+},
+  enumerable:false
+})
 ​
 ​
-Object.defineProperty(Array.prototype, 'insert', {
-  value: function(index, value) {
-    if (index >= this.length) {
-      this.push(value);
-    } else {
-      this.splice(index, 0, value);
-    }
-    return this;
-  },
-  enumerable: false
-});
+// Object.defineProperty(Array.prototype, 'insert', {
+//   value: function(index, value) {
+//     if (index >= this.length) {
+//       this.push(value);
+//     } else {
+//       this.splice(index, 0, value);
+//     }
+//     return this;
+//   },
+//   enumerable: false
+// });
 ​
