dc714b5cff274606a9c6a7adc05154ef8957627e
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-03 14:41:36 +0200

    Update solution - CodeHub

--- 7-kyu/array-array-array/array-array-array.js
+++ 7-kyu/array-array-array/array-array-array.js
@@ -1,20 +1,8 @@
+ 
 function explode(x){
- let a = x[0],b=x[1],time = null,final =[];
- if(typeof a == 'number'&& typeof b =='number') {
-     time=a+b;
- }else if(typeof a !== 'number' && typeof b === 'number') {
-     time = b;
- }else if(typeof b !== 'number' && typeof a === 'number'){
-     time =a;
- } else if(typeof a !== 'number' && typeof b !== 'number'){
-     return 'Void!'
- };
- if(time !== 0){
-     for(let i =0; i<time; i++){
-         final.push(x)
-     }
-     return final;
- }
- return time = []
- 
+let [q,w] = x;
+  if(typeof q === 'number' && typeof w === 'number') return new Array(q+w).fill(x);
+  if(typeof q === 'number') return new Array(q).fill(x);
+  if(typeof w === 'number') return new Array(w).fill(x);
+  return 'Void!'
 }
