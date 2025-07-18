eeb9f1949724a3e39881ace5bb6155247a764c79
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-18 22:02:38 +0200

    Update solution - CodeHub

--- 5-kyu/find-the-unique-string/find-the-unique-string.js
+++ 5-kyu/find-the-unique-string/find-the-unique-string.js
@@ -1,8 +1,17 @@
-​
 const findUniq =(arr)=>{
-    let other = arr.map(val=>[...new Set(val.toLowerCase())].sort().join(''))
-​
-    let unique = other.filter(val=>other.indexOf(val)== other.lastIndexOf(val)).join('');
-    return arr[other.indexOf(unique)]
+   let sorted = arr.map(val=>{
+       let unique = [];
+       for(let i of val.toLowerCase().split('').sort().join``){
+           if(!unique.includes(i)){
+               unique.push(i)
+           }
+       }
+       return unique.join``
+   })
+   for(let element of sorted){
+       if(sorted.indexOf(element)== sorted.lastIndexOf(element)){
+           return arr[sorted.indexOf(element)]
+       }
+   }
 }
 ​
