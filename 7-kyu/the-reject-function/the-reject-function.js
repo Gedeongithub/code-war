cf1389aeb7590dcb976d37ad365fac7f8dbd8900
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-18 22:30:31 +0200

    Update solution - CodeHub

--- 7-kyu/the-reject-function/the-reject-function.js
+++ 7-kyu/the-reject-function/the-reject-function.js
@@ -1,1 +1,10 @@
-const reject =(array, predicate) =>array.filter(val=> !predicate(val))
+function fSquared (arr) {
+ return filter(val=>{
+   let sum =0
+   if(typeof val !== 'number') return undefined;
+   if(val*val % 2 == 0){
+     sum +=val
+   }
+   return sum
+ })
+};
