19a4b1f1d579a88761ec82fcaf537bcd73af9321
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-18 22:44:50 +0200

    Update solution - CodeHub

--- 7-kyu/the-reject-function/the-reject-function.js
+++ 7-kyu/the-reject-function/the-reject-function.js
@@ -1,11 +1,8 @@
- 
 function fSquared (arr) {
- return arr.filter(val=>{
-   let sum =0
-   if(typeof val !== 'number') return undefined;
-   if(val*val % 2 == 0){
-     sum +=val
-   }
-   return sum
- })
+let match = 0;
+ for(let i of arr){
+     if(typeof i !== 'number') return undefined;
+    match+=i*i
+ }
+ return match
 };
