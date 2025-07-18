c68b25092bdd6f09d2ac316ee102aef76f4007be
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-18 22:47:50 +0200

    Update solution - CodeHub

--- 7-kyu/the-reject-function/the-reject-function.js
+++ 7-kyu/the-reject-function/the-reject-function.js
@@ -2,7 +2,9 @@ function fSquared (arr) {
 let match = 0;
  for(let i of arr){
      if(typeof i !== 'number') return undefined;
-    match+=i*i
+    if(i*i % 2 == 0){
+        match+=i*i
+    }
  }
  return match
 };
