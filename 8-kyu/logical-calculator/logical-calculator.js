a2445fa2ebd26c60b1fe402e5df9d5d2bda8086e
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-23 10:22:35 +0200

    Update solution - CodeHub

--- 8-kyu/logical-calculator/logical-calculator.js
+++ 8-kyu/logical-calculator/logical-calculator.js
@@ -1,6 +1,6 @@
 function logicalCalc(array, op){
   let result = array[0];
-  for(let val of array.slice(-1)){
+  for(let val of array.slice(1)){
     if(op==='AND') result = result && val;
     if(op=== 'XOR') result = result !== val
     if(op === 'OR') result = result || val
