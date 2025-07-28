5beb0ce84d3cfee897b7ed4c19b41f8fa8ee7479
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-28 11:44:49 +0200

    Add solution - CodeHub

---
+++ 7-kyu/move-zeros/move-zeros.js
@@ -0,0 +1,8 @@
+function move_zeros(arrNum, isRight){
+let zeros = arrNum.filter(val=>val==0),nonZeros = arrNum.filter(val=>val!==0);
+  if(isRight){
+    return [...nonZeros,...zeros]
+  }else{
+    return [...zeros,...nonZeros]
+  }
+}
