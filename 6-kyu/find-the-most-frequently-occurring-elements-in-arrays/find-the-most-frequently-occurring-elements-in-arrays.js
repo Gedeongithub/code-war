c4f2e6298b4c3dbf0ffc2f88f8f928c8f3fb3900
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-29 18:55:22 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-most-frequently-occurring-elements-in-arrays/find-the-most-frequently-occurring-elements-in-arrays.js
+++ 6-kyu/find-the-most-frequently-occurring-elements-in-arrays/find-the-most-frequently-occurring-elements-in-arrays.js
@@ -1,15 +1,39 @@
- 
+// function getMostFrequent(json) {
+//     return json.temperature.map(val=>{
+//             let frequencyMap ={};
+//             for(let temp of val ){
+//               frequencyMap[temp]=(frequencyMap[temp]||0)+1
+//             }
+//             let max = Math.max(...Object.values(frequencyMap));
+//             let mostFrequent = []
+//             for(let key in frequencyMap){
+//               if(frequencyMap[key]=== max) mostFrequent.push(Number(key))
+//           }
+//         return Math.max(...mostFrequent)
+//     })
+// } 
+​
+​
 function getMostFrequent(json) {
-    return json.temperature.map(val=>{
-            let frequencyMap ={};
-            for(let temp of val ){
-              frequencyMap[temp]=(frequencyMap[temp]||0)+1
+    return json.temperature.map(dayTemperatures => {
+        // Count frequency of each temperature
+        let frequencyMap = {};
+        for (let temp of dayTemperatures) {
+            frequencyMap[temp] = (frequencyMap[temp] || 0) + 1;
+        }
+        
+        // Find the maximum frequency
+        let maxFrequency = Math.max(...Object.values(frequencyMap));
+        
+        // Find all temperatures with maximum frequency
+        let mostFrequentTemps = [];
+        for (let temp in frequencyMap) {
+            if (frequencyMap[temp] === maxFrequency) {
+                mostFrequentTemps.push(Number(temp));
             }
-            let max = Math.max(...Object.values(frequencyMap));
-            let mostFrequent = []
-            for(let key in frequencyMap){
-              if(frequencyMap[key]=== max) mostFrequent.push(Number(key))
-          }
-        return Math.max(...mostFrequent)
-    })
-} 
+        }
+        
+        // Return the highest temperature in case of tie
+        return Math.max(...mostFrequentTemps);
+    });
+}
