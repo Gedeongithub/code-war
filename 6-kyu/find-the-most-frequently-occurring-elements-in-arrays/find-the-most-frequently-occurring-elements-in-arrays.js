5e55fd0f3ed5268e386232475e1260c106ba1acb
Gedeon DUFITIMANA <123157177+Gedeongithub@users.noreply.github.com>
2025-07-29 18:52:55 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-most-frequently-occurring-elements-in-arrays/find-the-most-frequently-occurring-elements-in-arrays.js
+++ 6-kyu/find-the-most-frequently-occurring-elements-in-arrays/find-the-most-frequently-occurring-elements-in-arrays.js
@@ -1,13 +1,14 @@
 function getMostFrequent(json) {
     return json.temperature.map(val=>{
-            let obj ={};
-            for(let element of val.sort((a,b)=>a-b) ){
-              obj[element]=(obj[element]||0)+1
+            let frequencyMap ={};
+            for(let temp of val ){
+              frequencyMap[temp]=(frequencyMap[temp]||0)+1
             }
-            let max = Math.max(...Object.values(obj));
-            for(let key in obj){
-              if(obj[key]=== max) return key
+            let max = Math.max(...Object.values(frequencyMap));
+            let mostFrequent = []
+            for(let key in frequencyMap){
+              if(obj[key]=== max) mostFrequent.push(Number(key))
           }
-    }).map(val=>Number(val))
-//   return obj[max] [ 22, 20, 22, 16, 19 ]
+        return Math.max(...mostFrequent)
+    })
 } 
